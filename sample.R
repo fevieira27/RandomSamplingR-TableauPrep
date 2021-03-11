573682199bafae2af8b9018febc5f297bfd31ce3
Felipe <60966475+fevieira27@users.noreply.github.com>
2021-03-11 10:58:37 +0000

    Update sample.R

--- sample.R
+++ sample.R
@@ -1,9 +1,12 @@
-random_samples <- function(dataset) {    
+random_samples <- function(dataset) {   
+  # Change the sampsize to the number of random samples required on the output.
   sampsize <- 100
   if(nrow(dataset) < sampsize){
     sampsize <- nrow(dataset)
   }
   set.seed(round(nrow(dataset)*100/sampsize,digits=2)/100)
   out <- dataset[sample(nrow(dataset), sampsize, replace = FALSE, prob = NULL),]
+  # The row below returns the sample number to a field called sample_id that should have been previously created in Tableau Prep
+  out$sample_id <- 1:nrow(out)
   return(data.frame(out))
 }
